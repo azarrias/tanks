e4e4cbc63f2ac3a531a988aeaab2be669fb5bd29
azarrias <adolfo.zarrias@gmail.com>
2020-09-15 11:15:54 +0200

    Create circular slider for the tanks UI

--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 
+// make sure that the UI stays in the same rotation that it was at the start
 public class UIDirectionControl : MonoBehaviour
 {
     public bool m_UseRelativeRotation = true;  
 
-
     private Quaternion m_RelativeRotation;     
-
 
     private void Start()
     {
+        // find the local rotation of the canvas
         m_RelativeRotation = transform.parent.localRotation;
     }
 
-
     private void Update()
     {
+        // set the current rotation to that local rotation of the canvas
         if (m_UseRelativeRotation)
             transform.rotation = m_RelativeRotation;
     }
