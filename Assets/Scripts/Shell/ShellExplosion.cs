77df889c60373cf48f7750ce07defbd2c57a3ebb
azarrias <adolfo.zarrias@gmail.com>
2020-09-15 17:21:16 +0200

    Create prefab for the projectiles and add behavior and particle system for explosions

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -2,30 +2,59 @@
 
 public class ShellExplosion : MonoBehaviour
 {
-    public LayerMask m_TankMask;
+    public LayerMask m_TankMask;                // layer that will be affected by the shell exploding
     public ParticleSystem m_ExplosionParticles;       
     public AudioSource m_ExplosionAudio;              
-    public float m_MaxDamage = 100f;                  
-    public float m_ExplosionForce = 1000f;            
-    public float m_MaxLifeTime = 2f;                  
-    public float m_ExplosionRadius = 5f;              
-
+    public float m_MaxDamage = 100f;            // maximum damage that the explosion can do
+    public float m_ExplosionForce = 1000f;      // force that will be done at the center of the explosion
+    public float m_MaxLifeTime = 2f;            // maximum lifetime for the shell
+    public float m_ExplosionRadius = 5f;        // how far from the shell is the explosion going to affect
 
     private void Start()
     {
         Destroy(gameObject, m_MaxLifeTime);
     }
 
-
     private void OnTriggerEnter(Collider other)
     {
         // Find all the tanks in an area around the shell and damage them.
-    }
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+        for (int i=0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!targetRigidbody)
+                continue;
 
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
+            if (!targetHealth)
+                continue;
+
+            float damage = CalculateDamage(targetRigidbody.position);
+            targetHealth.TakeDamage(damage);
+        }
+
+        // unparent the explosion particles, so that they keep playing after the shell is destroyed
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
+    }
 
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
+        Vector3 explosionToTarget = targetPosition - transform.position;
+        float explosionDistance = explosionToTarget.magnitude;
+
+        // calculate distance in the range from 0 to 1
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+        float damage = relativeDistance * m_MaxDamage;
+
+        // prevent corner case, where damage could be negative, if the tank center is outside the 
+        // explosion radius, but its collider is picked up by the overlap sphere
+        damage = Mathf.Max(0f, damage);
+        return damage;
     }
 }
